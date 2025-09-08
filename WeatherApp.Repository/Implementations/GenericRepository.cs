e4421e7760103e1a9e55a13f265573e39ee404a8
Freak4y <bojannn890@gmail.com>
2025-09-09 01:31:54 +0200

    feat(repository): add generic repository base CRUD

---
+++ WeatherApp.Repository/Implementations/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Repository.Data;
+using WeatherApp.Repository.Interfaces;
+
+namespace WeatherApp.Repository.Implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
