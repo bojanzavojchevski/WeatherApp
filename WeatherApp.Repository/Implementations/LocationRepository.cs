f9a49565a7509c7921470f68c09c8b83bb1c8186
Freak4y <bojannn890@gmail.com>
2025-09-09 01:42:10 +0200

    feat(repository): add LocationRepository with custom query

---
+++ WeatherApp.Repository/Implementations/LocationRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Domain.DomainModels;
+using WeatherApp.Repository.Data;
+using WeatherApp.Repository.Interfaces;
+
+namespace WeatherApp.Repository.Implementations
+{
+    public class LocationRepository : GenericRepository<Location>, ILocationRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LocationRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Location?> GetByCoordinatesAsync(decimal latitude, decimal longitude)
+        {
+            return await _context.Locations
+                .FirstOrDefaultAsync(loc => loc.Latitude == latitude && loc.Longitude == longitude);
+        }
+    }
+}
---
+++ WeatherApp.Repository/Interfaces/ILocationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Domain.DomainModels;
+
+namespace WeatherApp.Repository.Interfaces
+{
+    public interface ILocationRepository : IGenericRepository<Location>
+    {
+        Task<Location?> GetByCoordinatesAsync(decimal latitude, decimal longitude);
+    }
+}
